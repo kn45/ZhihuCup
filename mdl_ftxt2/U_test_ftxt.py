ec5f4a091f4ccba73efb4e0cdd60351aaf1f2461
kn45 <381404825@qq.com>
2017-07-04 15:41:06 +0800

    support model combo

--- mdl_ftxt2/U_test_ftxt.py
+++ mdl_ftxt2/U_test_ftxt.py
@@ -4,20 +4,16 @@ import fasttext
 import itertools
 import sys
 
-mdl = fasttext.load_model('mdl_fasttext_supervised.bin')
-#test_file = '../data_test/data_test.small.tsv'
+out_mode = 'prob' if len(sys.argv) > 1 and sys.argv[1] == 'prob' else 'cls'
+#test_file = '../data_test/data_test.1k.tsv'
 test_file = '../data_test/data_test.tsv'
-test_res = 'res_test/res_test.tsv'
-
-def grouper(n, iterable):
-    it = iter(iterable)
-    while True:
-        chunk = list(itertools.islice(it, n))
-        if not chunk:
-            return
-        yield chunk
-
+if out_mode == 'cls':
+    test_res = 'res_test/res_test.tsv'
+else:
+    test_res = 'res_test/res_test_prob.tsv'
 BATCH_SIZE = 256
+
+mdl = fasttext.load_model('mdl_fasttext_supervised.bin')
 with open(test_file) as f, open(test_res, 'w') as fo:
     # qid, label, title_chars, title_words
     iter_cnt = 0
@@ -32,12 +28,19 @@ with open(test_file) as f, open(test_res, 'w') as fo:
             test_feats = ' '
         feats_buff.append(test_feats)
         label_buff.append(test_label)
-        if iter_cnt % BATCH_SIZE == 0:
+        if iter_cnt % BATCH_SIZE != 0:
+            continue
+        if out_mode == 'cls':
             pred_raws = mdl.predict(feats_buff, 5)
             for pred_raw, test_label in zip(pred_raws, label_buff):
                 pred_label = ','.join(map(lambda x: x[9:], pred_raw))
                 print >> fo, test_label + '\t' + pred_label
-
-            iter_cnt = 0
-            feats_buff = []
-            label_buff = []
+        if out_mode == 'prob':
+            pred_raws = mdl.predict_proba(feats_buff, 15)
+            for pred_raw, test_label in zip(pred_raws, label_buff):
+                pred_label = ','.join(map(lambda t: t[0][9:]+':'+str(t[1]), pred_raw))
+                print >> fo, test_label + '\t' + pred_label
+        # reset buffer
+        iter_cnt = 0
+        feats_buff = []
+        label_buff = []
