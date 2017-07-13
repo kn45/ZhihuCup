3d0fc2bb3d97507cb0890567fc7751a3ccd44436
kn45 <381404825@qq.com>
2017-07-14 02:48:13 +0800

    support proba in pred

--- mdl_ftxt1/5_Pred.py
+++ mdl_ftxt1/5_Pred.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
 import fasttext
+import sys
+
+out_mode = 'prob' if len(sys.argv) > 1 and sys.argv[1] == 'prob' else 'cls'
+pred_file = '../data_all/question_eval_set.txt'
+if out_mode == 'cls':
+    pred_res = 'res_pred/res_pred.csv'
+else:
+    pred_res = 'res_pred/res_pred_prob.tsv'
 
 mdl = fasttext.load_model('mdl_fasttext_supervised.bin')
-
-pred_file = '../data_all/question_eval_set.txt'
-pred_res = 'res_pred/res_pred.csv'
 
 with open(pred_file) as f, open(pred_res, 'w') as fo:
     # qid, label, title_chars, title_words
@@ -15,10 +20,12 @@ with open(pred_file) as f, open(pred_res, 'w') as fo:
         pred_feats = ' '.join(flds[2].split(','))
         if len(pred_feats) == 0:
             pred_feats = ' '
-        try:
+        if out_mode == 'cls':
             pred_raw = mdl.predict([pred_feats], 5)
-        except:
-            sys.stderr.write(str(n) + '\t' + line)
-            sys.exit(1)
-        pred_label = ','.join(map(lambda x: x[9:], pred_raw[0]))
-        print >> fo, pred_id + ',' + pred_label
+            pred_label = ','.join(map(lambda x: x[9:], pred_raw[0]))
+            print >> fo, pred_id + ',' + pred_label
+        if out_mode == 'prob':
+            pred_raws = mdl.predict_proba([pred_feats], 15)
+            for pred_raw in pred_raws:
+                pred_label = ','.join(map(lambda t: t[0][9:]+':'+str(t[1]), pred_raw))
+                print >> fo, pred_id + '\t' + pred_label
