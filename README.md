2ff4284c125102314ea4ec4549866d381771ad09
kn45 <381404825@qq.com>
2017-07-27 11:09:13 +0800

    update cnn1 offline test res

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 | ftxt2 | fasttext with doc words | 0.64/0.27/0.19 | NA |
 | ftxt3 | bagging of ftxt1 & ftxt2 | 1.2/0.5/0.35 | 0.35 |
 | ftxt4 | like ftxt1, use inst with single label to train | 0.89/0.36/0.26 | NA |
-| cnn1  | cnn         | NA                | NA           |
+| cnn1  | cnn with title words | 1.09/0.45/0.32 | NA |
 | rnn1  | lstm with title words | 1.23/0.50/0.36 | 0.36 |
 | rnn2  | lstm with title words + doc words | 1.13/0.47/0.33 | 0.34 |
 | ftxt5 | fasttext with title words + doc words | 1.14/0.48/0.34 | 0.34 |
--- mdl_cnn1/4_Train.py
+++ mdl_cnn1/4_Train.py
@@ -4,14 +4,17 @@
 import numpy as np
 import tensorflow as tf
 import sys
-sys.path.append('../utils')
-sys.path.append('/Users/tchen/projects/TFModels/cnn_classifier')
+sys.path.append('../../MLFlow/utils')
+sys.path.append('../../TFModels/cnn_classifier')
 import dataproc
 from text_cnn import TextCNNClassifier
 
 NCLASS = 1999
 NWORDS = 411721
-SEQ_LEN = 150
+SEQ_LEN = 30
+MAX_ITER = 30000
+MODEL_CKPT_DIR = './model_ckpt/model.ckpt'
+LABEL_REPR='dense'
 
 def inp_fn(data):
     inp_x = []
@@ -24,39 +27,42 @@ def inp_fn(data):
         inp_x.append(dataproc.zero_padding(feats, SEQ_LEN))
     return np.array(inp_x), np.array(inp_y)
 
-def get_fbatch(fp, batch_size=1):
-    out = []
-    for line in fp:
-        out.append(line.rstrip('\n'))
-        if len(out) >= batch_size:
-            break
-    return out
-
 train_file = 'feat_train/trnvld_feature.tsv'
-train_fp = open(train_file)
+freader = dataproc.BatchReader(train_file, max_epoch=1)
 
 mdl = TextCNNClassifier(
     seq_len=SEQ_LEN,
-    emb_dim=128,
+    emb_dim=256,
     nclass=NCLASS,
     vocab_size=NWORDS,
     filter_sizes=[3, 4, 5],
     nfilters=128,
     reg_lambda=0.0,
+    label_repr=LABEL_REPR,
     lr=1e-3)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
-while mdl.global_steps < 1000:
-    batch_data = get_fbatch(train_fp, 64)
-    #print batch_data[0]
-    if len(batch_data) <= 0:
+sess.run(tf.local_variables_initializer())
+metrics = ['loss', 'accuracy']
+niter = 0
+
+while niter < MAX_ITER:
+    niter += 1
+    batch_data = freader.get_batch(256)
+    if not batch_data:
         break
     train_x, train_y = inp_fn(batch_data)
     mdl.train_step(sess, train_x, train_y)
-    #print train_x[0]
-    #print train_y[0][590:600]
-    print mdl.eval_step(sess, train_x, train_y)
+    train_eval = 'SKIP'
+    test_eval = 'SKIP'
+    if niter % 50 == 0:
+        train_eval = mdl.eval_step(sess, train_x, train_y, metrics)
+    # if niter % 50 == 0:
+    #     test_eval = mdl.eval_step(sess, test_x, test_y, metrics)
+    print niter, 'train:', train_eval, 'test:', test_eval
 
-train_fp.close()
+save_path = mdl.saver.save(sess, MODEL_CKPT_DIR, global_step=mdl.global_step)
+print "model saved:", save_path
+
 sess.close()
