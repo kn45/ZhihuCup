5487def65d2d90f6d6d55a526b98d3967b53a2a3
kn45 <381404825@qq.com>
2017-08-28 16:51:13 +0800

    use pretrained emb

--- mdl_cnn2/4_Train.py
+++ mdl_cnn2/4_Train.py
@@ -10,9 +10,23 @@ from text_cnn import TextCNNClassifier
 
 NCLASS = 1999
 NWORDS = 411721
-SEQ_LEN = 30
+SEQ_LEN = 100
 MAX_ITER = 30000
 MODEL_CKPT_DIR = './model_ckpt/model.ckpt'
+EMB_FILE = '../data_all/word_embedding.txt'
+train_file = 'feat_train/trnvld_feature.tsv'
+test_file = 'feat_test/test_feature.tsv'
+
+
+def load_embedding(embf, vocab_size, emb_size):
+    emb = [[]] * vocab_size  # create an empty word_embedding list.
+    emb[0] = np.zeros(emb_size)  # 'PAD'
+    with open(embf) as f:
+        for nl, line in enumerate(f):
+            vec = map(float, line.rstrip(' \n').split(' ')[1:])
+            emb[nl+1] = np.array(vec)
+    return np.array(emb)
+
 
 def inp_fn(data):
     inp_x = []
@@ -27,19 +41,20 @@ def inp_fn(data):
         inp_x.append(dataproc.zero_padding(feats, SEQ_LEN))
     return np.array(inp_x), np.array(inp_y)
 
-train_file = 'feat_train/trnvld_feature.tsv'
-freader = dataproc.BatchReader(train_file, max_epoch=1)
+
+train_reader = dataproc.BatchReader(train_file, max_epoch=1)
+test_reader = dataproc.BatchReader(test_file, max_epoch=1)
 
 mdl = TextCNNClassifier(
     seq_len=SEQ_LEN,
     emb_dim=256,
     nclass=NCLASS,
     vocab_size=NWORDS,
-    filter_sizes=[3, 4, 5],
-    nfilters=128,
+    filter_sizes=[2, 3, 4, 5, 6],
+    nfilters=256,
     reg_lambda=0.0,
     multi_label=True,
-    lr=1e-3)
+    lr=0.005)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
@@ -47,9 +62,11 @@ sess.run(tf.local_variables_initializer())
 metrics = ['loss']
 niter = 0
 
+pretrained_emb = load_embedding(EMB_FILE, NWORDS, 256)
+mdl.assign_embedding(sess, pretrained_emb)
 while niter < MAX_ITER:
     niter += 1
-    batch_data = freader.get_batch(256)
+    batch_data = train_reader.get_batch(256)
     if not batch_data:
         break
     train_x, train_y = inp_fn(batch_data)
@@ -58,8 +75,10 @@ while niter < MAX_ITER:
     test_eval = 'SKIP'
     if niter % 50 == 0:
         train_eval = mdl.eval_step(sess, train_x, train_y, metrics)
-    # if niter % 50 == 0:
-    #     test_eval = mdl.eval_step(sess, test_x, test_y, metrics)
+    if niter % 100 == 0:
+        test_x, test_y = inp_fn(test_reader.get_batch(1024))
+        test_eval = mdl.eval_step(sess, test_x, test_y, metrics)
+
     print niter, 'train:', train_eval, 'test:', test_eval
 
 save_path = mdl.saver.save(sess, MODEL_CKPT_DIR, global_step=mdl.global_step)
